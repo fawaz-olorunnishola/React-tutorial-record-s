0e30a34617c7ff54071c314ab28d5750073875a2
Fawas Olorunnishola <fawasolorunnishola9@gmail.com>
2023-12-06 23:16:14 +0100

    Update-first-react-course

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import  ReactDOM from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const BookList = () => {
+    return <section>
+    <Book/>
+    <Book/>
+    <Book/>
+    <Book/>
+    <Book/>
+    <Book/>
+    <Book/>
+    </section>
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Book = () => {
+    return <article>
+        <Image/>
+        <Title/>
+        <Author/>
+    </article>
+}
+
+const Image = () => 
+    <img 
+        src='Curious-mind.jpg' 
+        alt='Interesting Facts For Curious Minds' 
+/>
+const Title = () => <h2>Interesting Facts For Curious Minds</h2>
+const Author = () => {
+       return <h4>Jordan Moore</h4>
+}
+const root = ReactDOM.createRoot(document.getElementById
+('root'));
+
+root.render(<BookList />);
