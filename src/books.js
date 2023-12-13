879f65039c4bd0968995ecd2bf6b8083cbd7e5bb
Fawas Olorunnishola <fawasolorunnishola9@gmail.com>
2023-12-13 23:27:26 +0100

    Updated this days

---
+++ src/books.js
@@ -0,0 +1,26 @@
+import img1 from './images/Curious-mind.jpg';
+import img2 from './images/Too-late.PNG';
+import img3 from './images/The-gray-mountain.PNG';
+
+export const books = [
+
+    {
+        author: 'Jordan Moore',
+        title: 'Interesting Facts For Curious Minds',
+        img: img1,
+        id: 1,
+    },
+    {
+        author: 'Collen Hoover',
+        title: 'Too Late',
+        img: img2,
+        id: 2,
+    },
+    {
+        author: 'John Grisham',
+        title: 'Gray Mountain',
+        img: img3,
+        id: 3,
+    },
+]; 
+
--- src/index.js
+++ src/index.js
@@ -2,51 +2,24 @@ import React from 'react';
 import  ReactDOM from 'react-dom/client';
 
 import './index.css';
-
-const books = [
-    {
-        author: 'Jordan Moore',
-        title: 'Interesting Facts For Curious Minds',
-        img: 'images/Curious-mind.jpg',
-        id: 1,
-    },
-    {
-        author: 'Collen Hoover',
-        title: 'Too Late',
-        img: 'images/Too-late.PNG',
-        id: 2,
-    },
-] 
+import { books } from './books';
+import Book from './Book';
+ 
 
 const BookList = () => {
-    const getBook = (id) => {
-        const book = books.find((book)=> book.id === id )
-        console.log(book)
-    };
-    getBook(2);
-    return (
-    <section className='booklist'>
-        
-        {books.map((book)=>{
-        return <Book {...book} key={book.id} getBook={getBook} />;
-    })}
+return (
+    <>
+    <h1>Amazon Best Sellers</h1>
+        <section className='booklist'>
+        {books.map((book, index)=>{
+            return <Book {...book} key={book.id} number={index} />;
+        })}
     </section>
+        </>
     );
 };
 
 
-const Book = (props) => {
-    const { img, title, author, getBook, id} = props;
-    // console.log(props);
-return ( 
-    <article className='book'>  
-     <img src={img} alt={title} />
-        <h2>{title}</h2>
-        <button onClick={getBook(id)}>Click me </button>
-         <h4>{author}</h4>
-    </article>
-    );
-};
 
 const root = ReactDOM.createRoot(document.getElementById
 ('root'));
